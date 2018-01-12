1fba4744a04dfa1133eb9636146c5b5205c59feb
John Le <john.le@bonniercorp.com>
2018-01-11 19:49:55 -0500

    Add webpack and babel-loader

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	entry: './js/main.js',
+  output: {
+  	path: path.resolve(__dirname, 'public', 'js'),
+    filename: 'app.js'
+  },
+  module: {
+  	loaders: [{
+    	test: /\.js$/,
+      loader: 'babel-loader',
+      query: {
+      	presets: ['es2015']
+    	}
+  	}]
+	},
+  stats: {
+  	colors: true
+  },
+  devtool: 'source-map'
+};
